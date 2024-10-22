79a107eb0b908b8d9ea5823fd0df21e45fa0db79
Arkadii <arkadii.malygin@gmail.com>
2024-10-22 13:13:13 +0300

    implement change in maptest

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -22,43 +22,12 @@ public final class MapSchema<T, V> extends BaseSchema<Map<T, V>> {
 
     public MapSchema shape(Map<String, BaseSchema<T>> schemaOfMap) {
         addPredicate("isRequiredShape", new Predicate<Map<T, V>>() {
-//            https://github.com/ArkadiiMalygin/java-project-78/blob/main/app/src/main/java
-//            /hexlet/code/schemas/MapSchema.java#L24-L55 на самом деле тут можно немного упростить:
-//            value -> {
-//                return schemas.entrySet().stream().allMatch(e -> {
-//                    Object v = ((Map) value).get(e.getKey());
-//                    return e.getValue().isValid(v);
-//                });
-//            }
             @Override
             public boolean test(Map<T, V> tvMap) {
-                for (Map.Entry<T, V> entry : tvMap.entrySet()) {
-                    var key = entry.getKey();
-                    var value = entry.getValue();
-                    switch (schemaOfMap.get(key).getClass().getSimpleName()) {
-                        case "StringSchema" :
-                            StringSchema schemaForStringValue = (StringSchema) schemaOfMap.get(key);
-                            if (!schemaForStringValue.isValid((String) value)) {
-                                return false;
-                            }
-                            break;
-                        case "NumberSchema" :
-                            NumberSchema schemaForNumberValue = (NumberSchema) schemaOfMap.get(key);
-                            if (!schemaForNumberValue.isValid((Integer) value)) {
-                                return false;
-                            }
-                            break;
-                        case "MapSchema" :
-                            MapSchema schemaForMapValue = (MapSchema) schemaOfMap.get(key);
-                            if (!schemaForMapValue.isValid((Map<T, V>) value)) {
-                                return false;
-                            }
-                            break;
-                        default:
-                            System.out.println("unexpected");
-                    }
-                }
-                return true;
+                    return schemaOfMap.entrySet().stream().allMatch(e -> {
+                    Object v = tvMap.get(e.getKey());
+                    return e.getValue().isValid((T) v);
+                });
             }
         });
         return this;
